50e39272b72ceb5d1cad35e31d0620a25faa3256
FernandoHeMa <193225@ids.upchiapas.edu.mx>
2022-05-20 22:19:07 -0500

    Change: to csr method now it gets the values manually instead of using the numpy library. Added: the method str  was overridden

--- main.py
+++ main.py
@@ -4,15 +4,44 @@ import numpy as np
 class SparseMatrix:
 
     def __init__(self, array: np.array) -> None:
-        self.array = array
         self.inter_represent: str = 'CSR'
-        self.ROW_INDEX, self.COL_INDEX = np.nonzero(self.array)
-        self.number_of_nonzero: int = len(self.COL_INDEX)  # Col index of nonzero values
-        self.V = self.array[np.nonzero(self.array)]  # Non zero values
-        print("ROW:", self.ROW_INDEX)
-        print("COL:", self.COL_INDEX)
-        print("VALUES:", self.V)
+        self.array = array
+        self.number_of_rows, self.number_of_columns = np.shape(self.array)
+        self.current_representation: dict = self.to_csr()
+        self.number_of_nonzero = len(self.current_representation)
+
+    def to_csr(self) -> dict:
+        output: dict = dict()
+
+        for row in range(self.number_of_rows):
+            for column in range(self.number_of_columns):
+                if self.array[row][column] != 0:
+                    output[(row, column)] = self.array[row][column]
+        return output
+
+    def to_csc(self) -> dict:
+        output: dict = dict()
+        for row in range(self.number_of_rows):
+            for column in range(self.number_of_columns):
+                if self.array[row][column] != 0:
+                    output[(row, column)] = self.array[row][column]
+        return output
+
+    def __str__(self) -> str:
+        output = f"Representation: {self.inter_represent}\nNonzero = {self.number_of_nonzero}\n"
+        for key, value in self.current_representation.items():
+            output += f"{key} : {value}\n"
+        return output
 
 
-x = np.array([[3, 0, 0], [0, 4, 0], [5, 6, 0]])
-test = SparseMatrix(x)
+def main():
+    x = np.array([[3, 0, 0, 6, 0, 3, 0],
+                  [0, 4, 0, 0, 0, 0, 5],
+                  [5, 6, 0, 0, 3, 0, 0],
+                  [0, 0, 0, 0, 1, 0, 0],
+                  [1, 0, 0, 9, 0, 0, 0]])
+    sparse_matrix = SparseMatrix(x)
+    print(sparse_matrix)
+
+if __name__ == '__main__':
+    main()
